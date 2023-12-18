59ceffe0299dd2b5919e4cfe8f71430a8b52bf4b
AndreaBevilacqua <andrebevi2000@gmail.com>
2023-12-18 16:31:05 +0100

    added js stile

---
+++ js/script.js
@@ -0,0 +1,11 @@
+console.log('JS OK')
+
+/* 
+1 - dichiarare un variabile km e etá
+2 - chiedere all'utente km e etá 
+3 - Calcolare il prezzo del biglietto
+4 - Creare lo sconto per l'etá
+5 - Creare il prezzo finale
+6 - Stampare in pagina il prezzo
+*/
+
